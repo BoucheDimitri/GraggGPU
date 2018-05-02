de61f7676487e354cbd52827d830154987432a46
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2018-05-02 09:54:26 +0200

    Tested square kernel, working

--- Tests.cu
+++ Tests.cu
@@ -15,6 +15,17 @@ void print_matrix(float *c, int n) {
  	}	
 }
 
+//Function to print a small vector of floats
+void print_vector(float *c, int n) {
+
+	for (int i=0; i<n; i++){
+
+		printf("%f     ", c[i]);
+
+    		printf("\n");
+ 	}	
+}
+
 
 
 // Fill c with arrow matrix generated from vectors a and b
@@ -72,7 +83,7 @@ __global__ void square_kernel(float *bGPU, float *bsqrGPU, int n){
 int main (void) {
 
 	// Declare vectors
-	float *a, *b, *c;
+	float *a, *b, *bsqr, *c;
 
 	// Size of arrow matrix
 	int n = 10;
@@ -81,9 +92,10 @@ int main (void) {
 	float gamma = 1;
 	
 	// Memory allocation
-	a = (float*)malloc((n-1)*sizeof(int));
-	b = (float*)malloc((n-1)*sizeof(int));
-	c = (float*)malloc(n*n*sizeof(int));
+	a = (float*)malloc((n-1)*sizeof(float));
+	b = (float*)malloc((n-1)*sizeof(float));
+	bsqr = (float*)malloc((n-1)*sizeof(float));
+	c = (float*)malloc(n*n*sizeof(float));
 
 	// Fill the vectors
 	for (int i=0; i<n; i++){
@@ -95,32 +107,43 @@ int main (void) {
 	}
 
 	// Fill c with arrow matrix generated from a and b
-	generate_arrow(a, b, c, n);
+	generate_arrow(a, b, c, gamma, n);
 
 	// Print c
-	print_matrix(c, n);
+	//print_matrix(c, n);
 
 	
 	// Declare vectors on GPU
-	int *aGPU, *bGPU, *cGPU;
+	float *aGPU, *bGPU, *bsqrGPU;
 
 	// Create memory space for vectors on GPU
 	cudaMalloc(&aGPU, (n-1)*sizeof(float));
 	cudaMalloc(&bGPU, (n-1)*sizeof(float));
-	
-	
+	cudaMalloc(&bsqrGPU, (n-1)*sizeof(float));
 
+	// Transfer on GPU
+	cudaMemcpy(aGPU, a, (n-1)*sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(bGPU, b, (n-1)*sizeof(float), cudaMemcpyHostToDevice);
 
+	//Test square kernel
+	square_kernel <<<1024, 512>>> (bGPU, bsqrGPU, n);
+
+	cudaMemcpy(bsqr, bsqrGPU, (n-1)*sizeof(float), cudaMemcpyDeviceToHost);
+	
+	print_vector(bsqr, n-1);
+	
 
 
 	// Free memory on GPU
 	cudaFree(aGPU);
-	cudaFree(aGPU);
+	cudaFree(bGPU);
+	cudaFree(bsqrGPU);
 
 
 	// Free memory on CPU
 	free(a);
 	free(b);
+	free(bsqr);
 	free(c);
 	
 }
