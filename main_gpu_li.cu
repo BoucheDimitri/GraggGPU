1066c961051d7ccb59e49e7c0551b5e4e329193f
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2018-05-12 23:01:20 +0200

    Advances in main_gpu_li

--- main_gpu_li.cu
+++ main_gpu_li.cu
@@ -11,103 +11,111 @@ Lokman A. Abbas-Turki
 
 // Compare function for qsort
 int compare_function(const void *a,const void *b) {
-	float *x = (float *) a;
-	float *y = (float *) b;
-if (*x < *y) return 1;
-else if (*x > *y) return -1;
-return 0;
+    float *x = (float *) a;
+		float *y = (float *) b;
+    if (*x < *y) return 1;
+    else if (*x > *y) return -1;
+    return 0;
 }
 
 void positive_part_vec(float *v, int n){
-	for (int i = 0; i<n; i++){
-
-		v[i] = max((float)0, v[i]);
-	}
+    for (int i = 0; i<n; i++){
+        v[i] = max((float)0, v[i]);
+	  }
 }
 
 // Generate gaussian vector using Box Muller
-void gaussian_vector(float *v, float mu, float sigma, int n){
-	for (int i = 0; i<n; i++){
-		float u1 = (float)rand()/(float)(RAND_MAX);
-		float u2 = (float)rand()/(float)(RAND_MAX);
-		v[i] = sigma * (sqrtf( -2 * logf(u1)) * cosf(2 * M_PI * u2)) + mu;
-	}
+void gaussian_vector(float *v, float mu, float sigma, int n){*
+
+    for (int i = 0; i<n; i++){
+		    float u1 = (float)rand()/(float)(RAND_MAX);
+		    float u2 = (float)rand()/(float)(RAND_MAX);
+		    v[i] = sigma * (sqrtf( -2 * logf(u1)) * cosf(2 * M_PI * u2)) + mu;
+	  }
 }
 
 
 //Function to print a small square matrix of floats on host
 void print_matrix(float *c, int n) {
 
-	for (int i=0; i<n; i++){
-
-    		for(int j=0; j<n; j++) {
-
-         		printf("%f     ", c[n * i + j]);
-        	}
-
-    		printf("\n");
- 	}
+    for (int i=0; i<n; i++){
+        for(int j=0; j<n; j++) {
+            printf("%f     ", c[n * i + j]);
+        }
+        printf("\n");
+ 	  }
 }
 
 //Function to print a small vector of floats on host
 void print_vector(float *c, int m, int n) {
 
-	for (int i=0; i<m; i++){
-
-		printf("%f     ", c[i]);
-
-    		printf("\n");
- 	}
+    for (int i=0; i<m; i++){
+        printf("%f     ", c[i]);
+        printf("\n");
+ 	  }
 }
 
 
-
 // Kernel for computing the square of a vector
-// We actually only need the square of b in the computations
-// Thus it is better to compute it once and for all
-// We also collect the squared norm of the vector
-__global__ void square_kernel(float *bGPU, float *bsqrGPU, float *bnorm, int n){
+// We actually only need z ** 2 in the computations and not z
+__global__ void square_kernel(float *zGPU, float *zsqrGPU, int n){
 
-	int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
 
-	while(idx < n){
-		bsqrGPU[idx] = bGPU[idx] * bGPU[idx];
-		atomicAdd(bnorm, bsqrGPU[idx]);
-		idx += gridDim.x * blockDim.x;
-	}
+    while(idx < n){
+        float zi = zGPU[idx];
+		    zsqrGPU[idx] = zi * zi;
+		    idx += gridDim.x * blockDim.x;
+	  }
 }
 
 
 // Device function for computing f (the spectral function) at a given point x
-__device__ float spectral_func(float *aGPU,
-			       float *bsqrGPU,
-			       float x,
-			       float gamma,
-			       int n) {
+__device__ float secfunc(float *dGPU, float *zsqrGPU, float x, float rho, int n) {
 
-	float sum = 0;
+    float sum = 0;
+    for (int i=0; i<n-1; i++){
+        sum += zsqrGPU[i] / (dGPU[i] - x);
+	  }
 
-	for (int i=0; i<n-1; i++){
-		sum += bsqrGPU[i] / (aGPU[i] - x);
-	}
-
-	return x - gamma + sum;
+    return x - rho + sum;
 }
 
 
 // Device function for computing f' (the prime derivative of the spectral function) at a given point x
-__device__ float spectral_func_prime(float *aGPU,
-			       	     float *bsqrGPU,
-			             float x,
-			             int n) {
+__device__ float secfunc_prime(float *dGPU, float *zsqrGPU, float x, int n) {
 
-	float sum = 0;
+    float sum = 0;
+    for (int i=0; i<n-1; i++){
+        int di = dGPU[i];
+		    sum += zsqrGPU[i] / ((di - x) * (di - x));
+    }
 
-	for (int i=0; i<n-1; i++){
+	  return 1 + sum;
+}
 
-		int ai_local = aGPU[i];
-		sum += bsqrGPU[i] / ((ai_local - x) * (ai_local - x));
-	}
 
-	return 1 + sum;
+// Device function for computing f'' (the second derivative of the spectral function)
+__device__ float secfunc_second(float *dGPU, float *zsqrGPU, float x, int n){
+    float sum = 0;
+
+		for (int i = 0; i < count; i++) {
+		    float di = dGPU[i];
+				sum += zsqrGPU[i] / ((di - x) * (di -x) * (di -x));
+		}
+
+		return 2 * sum;
+}
+
+//
+__device__ float discrimant_int(float a, float b, float c){
+
+    if (a <= 0) return (a - sqrtf(a * a - 4 * b * c)) / (2 * c);
+    else return (2 * b) / (a + sqrtf(a * a - 4 * b *c));
+}
+
+
+__device__ float discrimant_ext(float a, float b, float c){
+    if (a >= 0) return (a + sqrtf(a * a - 4 * b * c)) / (2 * c);
+    else return (2 * b) / (a - sqrtf(a * a - 4 * b *c));
 }
